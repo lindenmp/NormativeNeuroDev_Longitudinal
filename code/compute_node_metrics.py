759fd5def3ed052fdd5d33a9a896d318132ae642
Linden Parkes <lindenparkes@gmail.com>
2020-03-27 11:48:55 -0400

    Push before removal

--- code/compute_node_metrics.py
+++ code/compute_node_metrics.py
@@ -6,20 +6,28 @@
 # In[1]:
 
 
+# Essentials
 import os, sys, glob
-import seaborn as sns
 import pandas as pd
 import numpy as np
-import statsmodels.api as sm
+import nibabel as nib
+
+# Stats
 import scipy as sp
 from scipy import stats
+import statsmodels.api as sm
+import pingouin as pg
+
+# Plotting
+import seaborn as sns
 import matplotlib.pyplot as plt
+plt.rcParams['svg.fonttype'] = 'none'
 
 
 # In[2]:
 
 
-sys.path.append('/Users/lindenmp/Dropbox/Work/ResProjects/NormativeNeuroDev_Longitudinal/code/func/')
+sys.path.append('/Users/lindenmp/Dropbox/Work/ResProjects/neurodev_long/code/func/')
 from proj_environment import set_proj_env
 from func import update_progress
 
@@ -28,15 +36,12 @@ from func import update_progress
 
 
 exclude_str = 't1Exclude'
-parc_str = 'schaefer'
-parc_scale = 400
-parcel_names, parcel_loc, drop_parcels, num_parcels, yeo_idx, yeo_labels = set_proj_env(exclude_str = exclude_str,
-                                                                            parc_str = parc_str, parc_scale = parc_scale)
+parcel_names, parcel_loc, drop_parcels, num_parcels, yeo_idx, yeo_labels = set_proj_env(exclude_str = exclude_str)
 
 
 # ### Setup output directory
 
-# In[15]:
+# In[4]:
 
 
 print(os.environ['MODELDIR'])
@@ -63,7 +68,7 @@ df.head()
 # In[7]:
 
 
-metrics = ('ct',)
+metrics = ('ct', 'vol')
 
 
 # In[8]:
@@ -71,12 +76,14 @@ metrics = ('ct',)
 
 # output dataframe
 ct_labels = ['ct_' + str(i) for i in range(num_parcels)]
-df_node = pd.DataFrame(index = df.index, columns = ct_labels)
+vol_labels = ['vol_' + str(i) for i in range(num_parcels)]
+
+df_node = pd.DataFrame(index = df.index, columns = ct_labels + vol_labels)
 
 print(df_node.shape)
 
 
-# ## Load in cortical thickness data
+# ## Load in data
 
 # In[9]:
 
@@ -84,11 +91,46 @@ print(df_node.shape)
 CT = np.zeros((df.shape[0], num_parcels))
 
 for (i, (index, row)) in enumerate(df.iterrows()):
-    full_path = glob.glob(os.path.join(os.environ['CTDIR'], str(index[0]), '*' + str(index[1]), os.environ['CT_FILE_NAME']))[0]
-    ct = np.loadtxt(full_path)
+    file_name = os.environ['CT_NAME_TMP'].replace("bblid", str(index[0]))
+    file_name = file_name.replace("scanid", str(index[1]))
+    full_path = glob.glob(os.path.join(os.environ['CTDIR'], file_name))
+    if i == 0: print(full_path)    
+
+    ct = np.loadtxt(full_path[0])
     CT[i,:] = ct
     
 df_node.loc[:,ct_labels] = CT
+
+
+# In[11]:
+
+
+# subject filter
+subj_filt = np.zeros((df.shape[0],)).astype(bool)
+
+
+# In[12]:
+
+
+VOL = np.zeros((df.shape[0], num_parcels))
+
+for (i, (index, row)) in enumerate(df.iterrows()):
+    file_name = os.environ['VOL_NAME_TMP'].replace("bblid", str(index[0]))
+    file_name = file_name.replace("scanid", str(index[1]))
+    full_path = glob.glob(os.path.join(os.environ['VOLDIR'], file_name))
+    if i == 0: print(full_path)    
+
+    img = nib.load(full_path[0])
+    v = np.array(img.dataobj)
+    v = v[v != 0]
+    unique_elements, counts_elements = np.unique(v, return_counts=True)
+    if len(unique_elements) == num_parcels:
+        VOL[i,:] = counts_elements
+    else:
+        print(str(index) + '. Warning: not all parcels present')
+        subj_filt[i] = True
+    
+df_node.loc[:,vol_labels] = VOL
 
 
 # In[10]:
@@ -138,4 +180,44 @@ df_node.head()
 
 
 df_node.to_csv(os.path.join(os.environ['MODELDIR'], 'df_node_clean.csv'))
+
+
+# In[11]:
+
+
+df_node = pd.read_csv(os.path.join(os.environ['MODELDIR'], 'df_node_clean.csv'))
+
+
+# In[13]:
+
+
+df_node.set_index(['bblid','scanid'], inplace = True)
+
+
+# In[15]:
+
+
+df_node.head()
+
+
+# In[24]:
+
+
+from func import summarise_network
+
+df_system = summarise_network(df_node, parcel_loc, yeo_idx, metrics = ('ct',), method = 'mean')
+
+df_system = pd.concat((df_node.filter(regex = 'ct', axis = 1).mean(axis = 1).rename('ct'),df_system),axis = 1)
+
+
+# In[25]:
+
+
+df_system.to_csv('/Users/lindenmp/Dropbox/Work/git/python_cookbook/data/df_long_system.csv')
+
+
+# In[26]:
+
+
+df.to_csv('/Users/lindenmp/Dropbox/Work/git/python_cookbook/data/df_long_pheno.csv')
 
